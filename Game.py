a67672cafa5dfe04a37384d1045e4877eeb71e04
otmama223 <otmane.kably@gmail.com>
2025-07-22 16:23:21 -0500

    Creation

---
+++ Game.py
@@ -0,0 +1,21 @@
+import random
+
+class Game:
+    def __init__(self, nb_players=2):
+        self.nb_players = nb_players
+    
+
+    def start(self):
+        pass
+
+    def preflop(self):
+        pass
+
+    def flop(self):
+        pass
+    def turn():
+        pass
+    def river():
+        pass
+    def showdown():
+        pass
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PokerGame
+This is a game of Poker Texas Hold'em.
+This project is only a step that will allow me to create my first ai project. I intend to create a poker bot that I will train to win make it win every game.
